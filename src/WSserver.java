8c9ec8104bc9f43764a2e01410eb3d9b62817893
Sizov Sergei <Wolf-sf@mail.ru>
2013-01-23 15:20:28 +0600

    Server of MyGame alpha 0.0.1

---
+++ src/WSserver.java
@@ -0,0 +1,21 @@
+import javax.servlet.http.HttpServletRequest;
+import org.apache.catalina.websocket.StreamInbound;
+import org.apache.catalina.websocket.WebSocketServlet;
+
+public class WSserver extends WebSocketServlet  {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected StreamInbound createWebSocketInbound(String arg0,
+			HttpServletRequest arg1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
